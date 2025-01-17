fdef18c4580b84006895d09a12776c706389dc1d
junyeong <557987@naver.com>
2025-01-17 22:47:48 +0900

    new

--- src/main/java/demo/csecircle/controller/ClanController.java
+++ src/main/java/demo/csecircle/controller/ClanController.java
@@ -1,6 +1,7 @@
 package demo.csecircle.controller;
 
 import demo.csecircle.UserConst;
+import demo.csecircle.classification.ClanRecruit;
 import demo.csecircle.controller.form.ClanForm;
 import demo.csecircle.controller.form.ClanSearchForm;
 import demo.csecircle.domain.Clan;
@@ -37,7 +38,9 @@ public class ClanController {
     }
 
     @GetMapping("/clans/clanSave")
-    public String clanSave(@ModelAttribute ClanForm clanForm, Model model){
+    public String clanSave(@ModelAttribute ClanForm clanForm, Model model, @SessionAttribute(name = UserConst.LOGIN_MEMBER,required = false) Member member){
+        model.addAttribute("member",member);
+        model.addAttribute("clanForm", clanForm);
         return "clan/clanSave";
     }
 
@@ -46,6 +49,11 @@ public class ClanController {
         Clan clan = new Clan();
         clan.setClanName(clanForm.getClanName());
         clan.setLeaderName(clanForm.getLeaderName());
+        clan.setClanLocation(clanForm.getClanLocation());
+        clan.setTelNum(clanForm.getTelNum());
+        clan.setMeetingTime(clanForm.getMeetingTime());
+        clan.setDescription(clanForm.getDescription());
+        clan.setIsRecruit(ClanRecruit.NO);
         clanService.saveClan(clan);
         return "redirect:/clans";
     }
@@ -56,6 +64,7 @@ public class ClanController {
         model.addAttribute("clan", clan);
         return "clan/signup";
     }
+
     @PostMapping("/clans/signup/{clanId}")
     public String postSignup(@PathVariable Long clanId, @ModelAttribute ClanForm clanForm,
                              @SessionAttribute(name = UserConst.LOGIN_MEMBER,required = false) Member member){
